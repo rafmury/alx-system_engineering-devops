65658fc93512c2d6aac101e8d16c24d56113bfa4
rafmury <kariukimraphael@gmail.com>
2024-01-16 12:07:04 -0800

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-"""Module for task 0"""
-
+"""Module that consumes the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit."""
-    user_agent = "MyRedditBot/1.0"  # Modify this with your project information
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    headers = {"User-Agent": user_agent}
+    """Queries the Reddit API and returns the number of subscribers (not
+    active users, total subscribers) for a given subreddit.
 
-    sub_info = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if sub_info.status_code == 404:
-        return 0
-    elif sub_info.status_code >= 300:
-        # Handle other error cases if needed
-        return 0
+    If not a valid subreddit, return 0.
+    Invalid subreddits may return a redirect to search results. Ensure that
+    you are not following redirects.
 
-    return sub_info.json().get("data").get("subscribers")
+    Args:
+        subreddit (str): subreddit
 
+    Returns:
+        int: number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/'
+
+    url = '{}{}/about.json'.format(base_url, subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    results = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if results.status_code == 200:
+        return results.json()['data']['subscribers']
+    return 0
