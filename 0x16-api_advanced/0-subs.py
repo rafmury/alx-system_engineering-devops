e055638af2d4c27da311afce8a85438e513677cf
rafmury <kariukimraphael@gmail.com>
2024-01-16 12:04:41 -0800

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """Module for task 0"""
 
+import requests
+
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    to the subreddit."""
+    user_agent = "MyRedditBot/1.0"  # Modify this with your project information
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    headers = {"User-Agent": user_agent}
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    sub_info = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if sub_info.status_code == 404:
+        return 0
+    elif sub_info.status_code >= 300:
+        # Handle other error cases if needed
         return 0
 
     return sub_info.json().get("data").get("subscribers")
+
